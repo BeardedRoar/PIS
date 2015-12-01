77d083255751200d27ba820394152297d3726ead
Ulrika Uddeborg <ulrika@uddeborg.se>
2015-12-01 11:23:48 +0100

    Creating pointer now

--- Laboration4/Laboration4/queue.c
+++ Laboration4/Laboration4/queue.c
@@ -1,4 +1,16 @@
 #include "queue.h"
 #include <stdlib.h>
 
-/* Implementera interface från queue.h här */
+/* Creates a new (empty) queue */
+QueuePtr new_queue() {
+    QueuePtr queue = 0;
+    return queue;
+}
+
+
+void delete_queue(QueuePtr q);          	// tar bort kön helt och hållet
+void clear(QueuePtr q);                 	// tar bort köelementen men behåller kön
+int  size(QueuePtr q);						// räknar köns aktuella längd
+void add(QueuePtr q, int prio, DataPtr d);	// lägger in d på rätt plats 
+DataPtr get_first(QueuePtr q);				// avläser första dataelementet 
+void remove_first(QueuePtr q);				// tar bort första köelementet

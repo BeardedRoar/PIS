0e13b61e411118c91b78a7ba4c0e0e753ad5ce70
BeardedRoar <joeljoel224@hotmail.com>
2015-12-07 10:17:21 +0100

    made add-method

--- Laboration4/Laboration4/queue.c
+++ Laboration4/Laboration4/queue.c
@@ -3,14 +3,39 @@
 
 /* Creates a new (empty) queue */
 QueuePtr new_queue() {
-    QueuePtr queue = 0;
-    return queue;
+	QueuePtr queueP = (QueuePtr)malloc(sizeof(struct QueueElement));
+	
+	queueP->data = NULL;
+	queueP->prio = MAX_PRIO;
+	queueP->next = NULL;
+    return queueP;
 }
 
-
-void delete_queue(QueuePtr q);          	// tar bort kön helt och hållet
+// tar bort kön helt och hållet
+void delete_queue(QueuePtr q) {
+         	
+}
 void clear(QueuePtr q);                 	// tar bort köelementen men behåller kön
 int  size(QueuePtr q);						// räknar köns aktuella längd
-void add(QueuePtr q, int prio, DataPtr d);	// lägger in d på rätt plats 
-DataPtr get_first(QueuePtr q);				// avläser första dataelementet 
+
+// lägger in d på rätt plats 
+void add(QueuePtr q, int prio, DataPtr d) {
+	while(q->next && q->next->prio > prio) {
+		q = q->next;
+	}
+	QueuePtr nextP = (QueuePtr)malloc(sizeof(struct QueueElement));
+	nextP->data = d;
+	nextP->next = q->next;
+	nextP->prio = prio;
+	q->next = nextP;
+}
+
+// avläser första dataelementet 
+DataPtr get_first(QueuePtr q) {
+	if(q->next) {
+		return q->next->data;
+	} else {
+		return 0;
+	}
+}
 void remove_first(QueuePtr q);				// tar bort första köelementet

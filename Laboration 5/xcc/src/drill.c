56e1355a56ffd53a111d8d65754801fa42b8dc58
Ulrika Uddeborg <ulrika@uddeborg.se>
2015-12-15 08:45:29 +0100

    Merged

--- Laboration 5/xcc/src/drill.c
+++ Laboration 5/xcc/src/drill.c
@@ -28,6 +28,24 @@ void Outone(int bit) {
    }
 }
 
+/* Turns the workpiece one step clockwise, if the drill is in
+ the up position when the subroutine is called. If the drill is in the
+ down position the workpiece is not turned and two alarm signals are sent. */
+int Step(void) {
+   if (!(DrillStatus & 8)) {
+      Alarm(2);		//Give 2 alarm signals
+      return 0;
+   } else {
+      Outzero(0);	//Turn off possible workspace turning bit
+      Outone(1); 	//Enable clockwise turning
+      Outone(0);	//Turn workpiece
+      
+      hold((time_type)500);	//Wait for 0,5 s (500 ms)
+      
+      return 1;
+   }
+}
+
 /* Starts the drill engine and then waits for 1 second so that the drill
  gets the correct speed. */
 void MotorStart(void) {
@@ -42,11 +60,27 @@ void MotorStop(void) {
 
 /* Lowers the drill. */
 void DrillDown(void) {
-   
+   Outone(3);
 }
 
-void DrillUp(void);
-int Nstep(int);
+/* Pulls up the drill. */
+void DrillUp(void) {
+   Outzero(3);
+   hold((time_type)1000);
+}
+
+/* Rotates the workpiece n steps clockwise */
+int Nstep(int steps) {
+   int i = 0;
+   
+   for(i = 0; i < steps; i++) {
+      if (!Step()) {
+         return 0;	//Alarm has sounded, someting went wrong.
+      }
+   }
+   return 1;
+}
+
 int DrillDownTest(void){
 	int i;
 	for (i = 0; i < 20; i++){
@@ -59,7 +93,18 @@ int DrillDownTest(void){
 	return 0;
 }
 
-void Alarm(int);
+/* Gives N alarm signals of the length 1 s, with a pause of 0,5 s between each signal. */
+void Alarm(int n) {
+   int i = 0;
+   for(i = 0; i < n; i++) {
+      if (!i) {
+         hold((time_type)500);	//Wait for last alarm to finish
+      }
+      Outone(4);		//Turn on alarm
+      hold((time_type)1000);	//Delay for 1 s (1000 ms)
+      Outzero(4);		//Turn off alarm
+   }
+}
 
 int DrillHole(void){
 	unsigned int success;
@@ -77,4 +122,5 @@ int RefPos(void){
 	}
 	return 1;
 }
+
 void DoAuto(void);
